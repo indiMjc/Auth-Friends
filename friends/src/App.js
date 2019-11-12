c06573c01a4934615b78a0c3a1a97f2a2c3709ed
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-12 15:45:50 -0500

    Switch Route for /login setup with Links to /login and /protected in unordered list on homepage

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/protected">Privileged content</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/login" />
+      </Switch>
     </div>
   );
 }
